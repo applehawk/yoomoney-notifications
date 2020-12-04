3c7f005980bfec42ade97dbd5d4312d07541dc4a
Vlad Skipper <ivlad.skipper@gmail.com>
2020-12-04 23:17:05 +0500

    fix changes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+require('dotenv').config()
+
 const fs = require('fs')
-const https = require('https');
+const https = require('https')
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
 
-const bodyParser = require("body-parser");
-const yamoney_http = require('./yamoney-http-notifications.js')
-
-const MongoClient = require("mongodb").MongoClient;
-const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
+const yamoney_http = require('./yamoney-http-notifications')
+const payment = require('./payment')
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,9 +18,10 @@ app.use(bodyParser.json());
 app.post('/yamoney', yamoney_http('9c7v1ZlWYLQEmS3jtGyyTksy', function(err, body) {
     if (body.test_notification === 'true' || body.operation_id === 'test-notification') {
         console.log("This is test notification")
+        payment(body);
     } else {
         console.log("This is real payment")
-
+        payment(body);
     }
 
     console.log(body); // here will be body of the notification
---
+++ payment.js
@@ -0,0 +1,60 @@
+const mongouser = process.env.MONGO_USER
+const mongopass = process.env.MONGO_PASS
+const mongourl = process.env.MONGO_URL
+//MONGO_URL="mongodb+srv://payment-script:XBnz0KVcr6kMx15Z@mlpayment-demo.mfbib.mongodb.net/?retryWrites=true&w=majority"
+
+const MongoClient = require("mongodb").MongoClient;
+const assert = require('assert');
+
+const url = "mongodb+srv://"+mongouser+":"+mongopass+"@"+mongourl+"/?retryWrites=true&w=majority"
+//here we handle ya-money p2p-incoming
+//https://yoomoney.ru/docs/wallet/using-api/notification-p2p-incoming
+const mongoClient = new MongoClient(url,
+    { useNewUrlParser: true, useUnifiedTopology: true });
+const dbName = 'usersdb'
+
+const processTransaction = (transaction) => {
+    console.log(url)
+    mongoClient.connect((err, client )=>{
+        assert.equal(null, err);
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection("payments");
+        collection.insertOne(transaction, function(err, result){
+            if(err){
+                return console.log(err);
+            }
+            console.log(result.ops);
+            client.close();
+        });
+    });
+}
+
+module.exports = function(body) {
+    let transaction = {
+        notification_type: body.notification_type, //notification_type: 'p2p-incoming',
+        bill_id: body.bill_id, //bill_id: '',
+        amount: body.amount, //amount: '404.27',
+        datetime: body.datetime, //datetime: '2020-12-04T17:42:28Z',
+        codepro: body.codepro, //codepro: 'false',
+        sender: body.sender, //sender: '41001000040',
+        operation_label: body.operation_label, //operation_label: '',
+        operation_id: body.operation_id, //operation_id: 'test-notification',
+        currency: body.currency, //currency: '643',
+        label: body.label, //label: ''
+        unaccepted: body.unaccepted,
+        //HTTPS only
+        lastname: body.lastname,
+        firstname: body.firstname,
+        fathersname: body.fathersname,
+        email: body.email,
+        phone: body.phone,
+        city: body.city,
+        street: body.street,
+        building: body.building,
+        suite: body.suite,
+        flat: body.flat,
+        zip: body.zip
+    };
+    return processTransaction(transaction)
+}
